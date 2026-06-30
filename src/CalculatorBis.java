31b1103cabc1f06f5d4887d9929122b008e85275
stefanovic80 <stefanovicgh@gmail.com>
2026-06-30 09:31:50 +0200

    Calculator is working with the four basic operations

--- src/CalculatorBis.java
+++ src/CalculatorBis.java
@@ -1,7 +1,32 @@
 package src;
 public class CalculatorBis{
+
+    public static float sum( float a, float b){
+        return a + b;
+    }
+
+    public static float diff( float a, float b){
+        return a - b;
+    }
+
+    public static float mult( float a, float b){
+        return a * b;
+    }
+
+    public static float div( float a, float b){
+        return a / b;
+    }
+
     public static void main(String[] args){
-        System.out.println("Cazzarola!");
+        float value;
+        CalculatorBis calc = new CalculatorBis();
+        calc.sum(5, 3);
+        calc.diff(5,3);
+
+        System.out.println("Sum is " + calc.sum(5, 3));
+        System.out.println("Diff is " + calc.diff(5, 3));
+        System.out.println("Mult is " + calc.mult(5, 3));
+        System.out.println("Div is " + calc.div(5, 3));
     }
 
 }
