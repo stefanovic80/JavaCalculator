8b8124cf306569e50f7dbdc96b708a48600c42a9
stefanovic80 <stefanovicgh@gmail.com>
2026-06-30 17:07:38 +0200

    Two calculators

--- src/CalculatorTris.java
+++ src/CalculatorTris.java
@@ -17,12 +17,16 @@ public class CalculatorTris{
         return a / b;
     }
 
+    /** Per eseguire le quattro operazioni su 10 e 5 digitare javac -d bin src/CalculatorTris.java 10 5
+    */
     public static void main(String[] args){
+        float a = Float.parseFloat(args[0]);
+        float b = Float.parseFloat(args[1]);
 
-        System.out.println("Sum is " + CalculatorTris.sum(5, 3));
-        System.out.println("Diff is " + CalculatorTris.diff(5, 3));
-        System.out.println("Mult is " + CalculatorTris.mult(5, 3));
-        System.out.println("Div is " + CalculatorTris.div(5, 3));
+        System.out.println("Sum is " + CalculatorTris.sum(a, b));
+        System.out.println("Diff is " + CalculatorTris.diff(a, b));
+        System.out.println("Mult is " + CalculatorTris.mult(a, b));
+        System.out.println("Div is " + CalculatorTris.div(a, b));
     }
 
 }
